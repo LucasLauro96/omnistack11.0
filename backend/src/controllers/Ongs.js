d661b49ed2261b9e0a75e5f269cbba2cf5cb1322
Lucas Lauro Costa <lucaskns1996@gmail.com>
2020-06-15 20:10:01 -0300

    Ended with error on mobile

--- backend/src/controllers/Ongs.js
+++ backend/src/controllers/Ongs.js
@@ -24,5 +24,13 @@ module.exports = {
         });
 
         return res.json({ id });
+    },
+
+    async delete(req, res){
+        const { id } = req.params;
+
+        await connection('ongs').where('id', id).delete();
+
+        return res.status(204).send();
     }
 }
--- mobile/src/pages/Incidents/index.js
+++ mobile/src/pages/Incidents/index.js
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Feather } from '@expo/vector-icons';
 import { useNavigation } from '@react-navigation/native'
 import { View, FlatList, Image, Text, TouchableOpacity } from 'react-native';
+
+import api from '../../services/api'
 
 import logoImg from '../../assets/logo.png'
 
@@ -9,10 +11,21 @@ import styles from './styles'
 
 export default function Incidents() {
     const navigation = useNavigation();
+    const [incidents, setIncidents] = useState([]);
 
     function navigateToDetail(){
         navigation.navigate('Detail');
     }
+
+    async function loadIncidents(){
+        const response = await api.get('incident');
+
+        setIncidents(response.data);
+    }
+
+    useEffect(() => {
+        loadIncidents()
+    }, []);
 
 
     return (
@@ -28,20 +41,20 @@ export default function Incidents() {
             <Text style={styles.description}>Escolha um caso abaixo e salve o dia</Text>
 
             <FlatList
-                data={[1, 2, 3]}
+                data={incidents}
                 style={styles.incidentList}
-                keyExtractor={incident => String(incident)}
+                keyExtractor={incident => String(incident.id)}
                 showsVerticalScrollIndicator={false}
-                renderItem={() => (
+                renderItem={({ item: incident }) => (
                     <View style={styles.incident}>
                         <Text style={styles.incidentProperty}>ONG:</Text>
-                        <Text style={styles.incidentValue}>APAD</Text>
+                        <Text style={styles.incidentValue}>{incident.name}</Text>
 
                         <Text style={styles.incidentProperty}>CASO:</Text>
-                        <Text style={styles.incidentValue}>Cadelinha atropelada</Text>
+                        <Text style={styles.incidentValue}>{incident.title}</Text>
 
                         <Text style={styles.incidentProperty}>VALOR:</Text>
-                        <Text style={styles.incidentValue}>R$ 120,00</Text>
+                        <Text style={styles.incidentValue}>R$ {incident.value}</Text>
 
                         <TouchableOpacity style={styles.detailsButton} onPress={navigateToDetail}>
                             <Text style={styles.detailsButtonText}>Ver mais detalhes</Text>
